eb33110593de0a02611ceb41e025aaee4e82c51b
Codrin <bacu.codrin2007@gmail.com>
2025-11-26 19:08:41 +0200

    Commit 2

--- Proceduri_Triggere_Functii.sql
+++ Proceduri_Triggere_Functii.sql
@@ -1,1 +1,107 @@
-	USE BazaGeo_DB;
+﻿	USE BazaGeo_DB;
+
+	-- Procedura: Inserare marfa noua
+	CREATE PROCEDURE usp_AddMarfa
+    @ID_Marfa VARCHAR(10),
+    @Denumire VARCHAR(100),
+    @ID_Tip VARCHAR(10),
+    @ID_Furnizor VARCHAR(10),
+    @Pret_unitar FLOAT,
+    @Stoc INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF NOT EXISTS (SELECT 1 FROM Furnizor WHERE ID_Furnizor = @ID_Furnizor)
+    BEGIN
+        RAISERROR('Furnizorul nu există!', 16, 1);
+        RETURN;
+    END
+
+    INSERT INTO Marfa(ID_Marfa, Denumire, ID_Tip, ID_Furnizor, Pret_unitar, Stoc)
+    VALUES (@ID_Marfa, @Denumire, @ID_Tip, @ID_Furnizor, @Pret_unitar, @Stoc);
+END
+GO
+
+    -- Procedura: Listarea Intrarilor cu furnizorul
+    CREATE PROCEDURE usp_GetIntrariByFurnizor
+    @ID_Furnizor VARCHAR(10)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT I.ID_Intrare, I.Data_intrare, F.Denumire_furnizor
+    FROM Intrari I
+    JOIN Furnizor F ON I.ID_Furnizor = F.ID_Furnizor
+    WHERE I.ID_Furnizor = @ID_Furnizor;
+END
+GO
+
+    -- Trigger: Actualizarea stocului cand se insereaza o pozitie
+    CREATE TRIGGER TR_UpdateStoc_OnInsertPozitie
+ON Pozitii_intrari
+AFTER INSERT
+AS
+BEGIN
+    UPDATE M
+    SET M.Stoc = M.Stoc + I.Cantitate
+    FROM Marfa M
+    JOIN inserted I ON M.ID_Marfa = I.ID_Marfa;
+END
+GO
+
+
+    -- Trigger: Nu permite stergerea furnizorilor daca exista marfa asociata
+    CREATE TRIGGER TR_BlockDeleteFurnizor
+ON Furnizor
+INSTEAD OF DELETE
+AS
+BEGIN
+    IF EXISTS (
+        SELECT 1 
+        FROM Marfa M 
+        JOIN deleted D ON M.ID_Furnizor = D.ID_Furnizor
+    )
+    BEGIN
+        RAISERROR('Nu poți șterge furnizorul deoarece există marfă asociată!', 16, 1);
+        RETURN;
+    END
+    
+    DELETE FROM Furnizor
+    WHERE ID_Furnizor IN (SELECT ID_Furnizor FROM deleted);
+END
+GO
+
+    -- Functie Scalara: Returneaza denumirea furnizorului dupa ID
+    CREATE FUNCTION fn_GetFurnizorName (@ID_Furnizor VARCHAR(10))
+RETURNS VARCHAR(50)
+AS
+BEGIN
+    DECLARE @Name VARCHAR(50);
+
+    SELECT @Name = Denumire_furnizor
+    FROM Furnizor
+    WHERE ID_Furnizor = @ID_Furnizor;
+
+    RETURN @Name;
+END
+GO
+
+    -- Exemplu
+    SELECT dbo.fn_GetFurnizorName('FU001');
+
+
+    -- Functie Tabelara: Lista marfa pentru un furnizor
+    CREATE FUNCTION fn_MarfaByFurnizor (@ID_Furnizor VARCHAR(10))
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT ID_Marfa, Denumire, Pret_unitar, Stoc
+    FROM Marfa
+    WHERE ID_Furnizor = @ID_Furnizor
+);
+GO
+
+    -- Exemplu
+    SELECT * FROM dbo.fn_MarfaByFurnizor('FU001');
